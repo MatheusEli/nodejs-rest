2803bca73827add6df044136340aa52e9dbead36
MatheusEli <matheuselisouzaferreira@hotmail.com>
2021-03-27 16:34:54 -0300

    adicionando req PATCH e DELETE

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,10 +1,29 @@
+const Atendimento = require('../models/atendimentos');
 module.exports = app => {
-    
     app.get('/atendimentos', (req, res) => {
-        res.send('Você está na rota de atendimentos e realizando um GET');
+        Atendimento.lista(res);
+    });
+
+    app.get('/atendimentos/:id', (req,res) => {
+        const id = parseInt(req.params.id);
+        Atendimento.buscaPorId(id, res);
     });
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body);
-        res.send('Você está na rota de atendimentos e realizando um POST');
+        const atendimento = req.body;
+
+        Atendimento.adiciona(atendimento, res);
+    });
+
+    app.patch('/atendimentos/:id', (req, res) =>{
+
+        const id = parseInt(req.params.id);
+        const valores = req.body;
+
+        Atendimento.altera(id, valores, res);
+    });
+
+    app.delete('/atendimentos/:id', (req,res) => {
+        const id = parseInt(req.params.id);
+        Atendimento.deleta(id, res);
     })
 }
